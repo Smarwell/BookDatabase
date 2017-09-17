5a96ef22248f12c08b2e45d7700ce0a2e0d68c28
Jacob Sacco <jacobsacco@tamu.edu>
2017-09-16 22:42:16 -0500

    added header stuff

--- BookDatabase.cpp
+++ BookDatabase.cpp
@@ -1,7 +1,73 @@
 
-#include<stdio.h>
+#include<iostream>
+#include<cstdlib>
+#include<string>
+#include<map>
+#include<vector>
 
-int main(){
-	printf("FUUUUUUCK\n");
+using namespace std;
+
+class Section;
+class Book;
+
+class Course {
+	string id;
+	string name;
+	map<int, Section*> sections;
+public:
+	Course(string id, string name)
+		:id(id), name(name){}
+	
+	void add_section(int section_num, Section* section){
+		sections[section_num] = section;
+	}
+};
+
+const bool REQUIRED = true;
+const bool OPTIONAL = false;
+
+class Section {
+	Course* parent;
+	int section_number;
+	vector<Book*> required_books;
+	vector<Book*> optional_books;
+public:
+	Section(Course* parent, int section_num)
+		:parent(parent), section_number(section_num){}
+	
+	void add_book(Book* book, bool required){
+		if(required){
+			required_books.push_back(book);
+		} else {
+			optional_books.push_back(book);
+		}
+	}
+	
+	void print_books(){
+		
+	}
+	
+	vector<Book*>* get_required(){
+		
+	}
+	
+	vector<Book*>* get_optional(){
+		
+	}
+};
+
+class Book {
+	long int isbn;
+	string title;
+	string author;
+	string date;
+	int edition;
+	map<string, double> type_costs;
+	vector<Section*> required_by;
+};
+
+
+int main() {
+	cout << "Fuck you\n";
 	return 0;
 }
